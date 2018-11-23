16a44ee86655e9681e9d43f48b0cd1ba893dd213
shuhei <kawashu0723@gmail.com>
2018-11-23 21:54:39 +0900

    get devices

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"log"
+	"github.com/google/gopacket/pcap"
+	"fmt"
 )
 
 func main() {
-	log.Println("main")
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, device := range devices {
+		fmt.Println("Name: " + device.Name)
+		fmt.Println("Description: " + device.Description)
+		for _, address := range device.Addresses {
+			fmt.Println("- IP: " + address.IP.String())
+			fmt.Println("- NetMask: " + address.Netmask.String())
+		}
+		fmt.Println("")
+	}
 }
