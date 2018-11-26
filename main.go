b7139608a49b48e9acca347c2806b0f8e586200a
shuhei <kawashu0723@gmail.com>
2018-11-26 16:10:59 +0900

    show ip layer

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/google/gopacket/pcap"
 	"fmt"
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 )
 
 func main() {
@@ -27,8 +28,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		fmt.Println(packet.String())
+		for _, layer := range packet.Layers() {
+			fmt.Println(layer.LayerType())
+		}
+		ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+		if ethernetLayer != nil {
+			fmt.Println("[*] Ethernet Layer")
+			eth, _ := ethernetLayer.(*layers.Ethernet)
+			fmt.Printf("\t%s -> %s\n", eth.SrcMAC, eth.DstMAC)
+		}
+		ipLayer := packet.Layer(layers.LayerTypeIPv4)
+		if ipLayer != nil {
+			fmt.Println("[*] IPv4 layer")
+			ip, _ := ipLayer.(*layers.IPv4)
+			fmt.Printf("\t%s -> %s\n", ip.SrcIP, ip.DstIP)
+		}
 	}
 }
